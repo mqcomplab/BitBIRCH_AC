4c580432fc85cf6e9b95ed82c65f006455f8c8af
Akash Surendran <152842908+Ak-Sure@users.noreply.github.com>
2025-09-17 02:31:55 +0000

    file upload

--- bb_utils/generate_plots.py
+++ bb_utils/generate_plots.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 # Load both CSV files
 
-def plot_overall_comparison(df1, df2, offset1, offset2): #To plot overall comparison across different/same offsets for different/same dataset. 
+def plot_overall_comparison(df1, df2, offset1, offset2):
 
     # Remove the rows where ratio is -1
     df1 = df1[df1['ratio'] != -1]
@@ -71,7 +71,7 @@ def plot_overall_comparison(df1, df2, offset1, offset2): #To plot overall compar
     plt.show()
     plt.close()
 
-def plot_by_threshold(df, offset, output_file): # To check the efficiency of AC retrieval for a dataset across different thresholds.
+def plot_by_threshold(df, offset, output_file):
 
     df = df[df['ratio'] != -1]
     df = df[(df['offset']==offset)]
@@ -109,7 +109,7 @@ def plot_by_threshold(df, offset, output_file): # To check the efficiency of AC 
     plt.close()
 
 
-def plot_comparison_by_threshold(df1, df2, offset1, offset2): # To compare the AC retrieval performance across different/same dataset at different/same offset.
+def plot_comparison_by_threshold(df1, df2, offset1, offset2):
 
     df1 = df1[df1['ratio'] != -1]
     df2 = df2[df2['ratio'] != -1]
@@ -173,7 +173,7 @@ def plot_comparison_by_threshold(df1, df2, offset1, offset2): # To compare the A
     plt.show()
     plt.close()
 
-def plot_num_molecule(df, fps=['ECFP', 'MACCS', 'RDKIT']): #Number of molecules in the top clusters
+def plot_num_molecule(df, fps=['ECFP', 'MACCS', 'RDKIT']): #Offset is not used now
     
 
     colors=plt.cm.viridis
@@ -200,7 +200,7 @@ def plot_num_molecule(df, fps=['ECFP', 'MACCS', 'RDKIT']): #Number of molecules 
         plt.savefig(f'Plots/Smooth/cluster_size_vs_number_{fp}.png', dpi=300)
         plt.show()
 
-def plot_prop_std(df, fps=['ECFP', 'MACCS', 'RDKIT']): # Property standard deviation in top clusters
+def plot_prop_std(df, fps=['ECFP', 'MACCS', 'RDKIT']): #Offset is not used now
         colors=plt.cm.viridis
 
         for fp in fps:
@@ -233,4 +233,105 @@ def plot_prop_std(df, fps=['ECFP', 'MACCS', 'RDKIT']): # Property standard devia
           plt.tight_layout()
           plt.savefig(f'Plots/Smooth/cluster_property_std_vs_number_{fp}.png', dpi=300)
           plt.show()
+
+
+def all_comparisons(df1, df2, offset1, offset2):
+    
+    df1 = df1[df1['ratio'] != -1]
+    df2 = df2[df2['ratio'] != -1]
+
+    dfa=df1[(df1['offset']==offset1)]
+    dfb=df2[(df2['offset']==offset1)]
+    dfc=df1[(df1['offset']==offset2)]
+    dfd=df2[(df2['offset']==offset2)]
+
+    fingerprints = ['ECFP', 'MACCS', 'RDKIT']
+
+    meansa = []
+    stdsa = []
+    meansb = []
+    stdsb = []
+    meansc = []
+    stdsc = []
+    meansd = []
+    stdsd = []
+
+    for fp in fingerprints:
+
+        # Dataset a
+        ratiosa = dfa[dfa['suffix'].str.endswith(fp)]['ratio']
+        meansa.append(ratiosa.mean())
+        stdsa.append(ratiosa.std())
+
+        # Dataset b
+        ratiosb = dfb[dfb['suffix'].str.endswith(fp)]['ratio']
+        meansb.append(ratiosb.mean())
+        stdsb.append(ratiosb.std())
+
+        # Dataset c
+        ratiosc = dfc[dfc['suffix'].str.endswith(fp)]['ratio']
+        meansc.append(ratiosc.mean())
+        stdsc.append(ratiosc.std())
+
+        # Dataset d
+        ratiosd = dfd[dfd['suffix'].str.endswith(fp)]['ratio']
+        meansd.append(ratiosd.mean())
+        stdsd.append(ratiosd.std())
+
+    plt.figure(figsize=(15, 9))
+    
+    x = np.arange(len(fingerprints))
+    width = 0.2
+
+    df1_recursive = df1['recursive'].unique()
+    df2_recursive = df2['recursive'].unique()
+
+    if len(df1_recursive) != 1 or len(df2_recursive) != 1:
+        raise ValueError("DataFrames must have a single unique value for 'recursive' column.")
+    df1_recursive = df1_recursive[0]
+    df2_recursive = df2_recursive[0]
+    if df1_recursive==False:
+        label1='Non-Recursive'
+    else:
+        label1='Recursive'
+
+    if df2_recursive==False:
+        label2='Non-Recursive'
+    else:
+        label2='Recursive'
+
+    # Define color schemes for each fingerprint type
+    fp_colors = {
+        'ECFP': ['#1f77b4', '#aec7e8', "#1B1EB6", "#7173c9"],    # Blues
+        'MACCS': ['#ff7f0e', '#ffbb78', "#f03709", "#fc9d90"],   # Oranges
+        'RDKIT': ['#2ca02c', '#98df8a', "#0e411f", "#5D8560"]    # Greens
+    }
+    
+    # Create the bars with new colors
+    bars1 = plt.bar(x - width, meansa, width, yerr=stdsa, capsize=5,
+                    color=[fp_colors[fp][0] for fp in fingerprints], 
+                    alpha=0.9, label=f'{label1}, Offset={offset1}')
+    
+    bars2 = plt.bar(x - 0, meansb, width, yerr=stdsb, capsize=5,
+                    color=[fp_colors[fp][1] for fp in fingerprints], 
+                    alpha=0.9, label=f'{label2}, Offset={offset1}')
+    
+    bars3 = plt.bar(x + width, meansc, width, yerr=stdsc, capsize=5,
+                    color=[fp_colors[fp][2] for fp in fingerprints],
+                    alpha=0.9, label=f'{label1}, Offset={offset2}')
+    
+    bars4 = plt.bar(x + 2*width, meansd, width, yerr=stdsd, capsize=5,
+                    color=[fp_colors[fp][3] for fp in fingerprints],
+                    alpha=0.9, label=f'{label2}, Offset={offset2}')
+    
+    plt.xlabel('Fingerprint', fontsize=12)
+    plt.ylabel('Mean Ratio', fontsize=12)
+    plt.title('Mean Ratio by Fingerprint Type', fontsize=14)
+    plt.xticks(x + width/2, fingerprints)
+    plt.legend()
+    plt.tight_layout()
+    plt.savefig(f'plots/all_comparisons_{label1}_{offset1}_{label2}_{offset2}.png')
+    plt.show()
+    plt.close()
+
 
